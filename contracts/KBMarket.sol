668ec041ba5b8da7250d521fc4f19163757819d5
gano <gen.nagano.cc20120701@gmail.com>
2023-01-21 08:23:12 +0900

    18.DApp Smart Contracts PartIII

--- contracts/KBMarket.sol
+++ contracts/KBMarket.sol
@@ -125,9 +125,26 @@ contract KBMarket is ReentrancyGuard {
     payable(owner).transfer(listingPrice);
   }
 
+  // function to fetchMarketItems - minting, buying and selling
+  // return the number of unsoled items
+  function fetchMarketTokens() public view returns (MarketToken[] memory) {
+    uint itemCount = _tokenIds.current();
+    uint unsoldItemCount = _tokenIds.current() - _tokensSold.current();
+    uint currentIndex = 0;
 
-
-  // function to fetchMarketItems 
+    // looping over the number of items created (if number has not been sold populate the array)
+    MarketToken[] memory items = new MarketToken[](unsoldItemCount);
+    
+    for (uint i = 0; i < itemCount; i++) {
+      if (idToMarketToken[i + 1].owner == address(0)) {
+        uint currentId = i + 1;
+        MarketToken storage currentItem = idToMarketToken[currentId];
+        items[currentIndex] = currentItem;
+        currentIndex += 1;
+      }
+    }
+    return items;
+  }
 
 }
 

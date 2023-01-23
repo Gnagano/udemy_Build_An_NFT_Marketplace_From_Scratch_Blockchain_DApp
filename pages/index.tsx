43bea26b2bd8378d6aa7c203f5f0c940e6963bbc
gano <gen.nagano.cc20120701@gmail.com>
2023-01-23 16:18:21 +0900

    27.Loading NFTs To The Market Function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,64 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
+import axios from "axios";
 
+// Web3
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
-import axios from "axios";
 import Web3Modal from "web3modal";
+
+// Contract
+import KBMarket from "../artifacts/contracts/KBMarket.sol/KBMarket.json";
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+
+// Config
+import { nftaddress, nftmarketaddress } from "../config";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [nfts, setNFTs] = useState<any[]>([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  const loadNFTs = async () => {
+    // What we want to load:
+    // ***provider, tokenContract, marketContract, data for our marketItems
+
+    const provider = new ethers.providers.JsonRpcProvider();
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
+    const marketContract = new ethers.Contract(
+      nftmarketaddress,
+      KBMarket.abi,
+      provider
+    );
+    const data = await marketContract.fetchMarketItems();
+
+    const items = await Promise.all(
+      data.map(async (i: any) => {
+        const tokenUri = await tokenContract.tokenURI(i.tokenId);
+        // We want get the token metadata - json
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          name: meta.data.name,
+          description: meta.data.description
+        };
+        return item;
+      })
+    );
+    setNFTs(items);
+    setLoadingState("loaded");
+  };
+
   return (
     <>
       <Head>
